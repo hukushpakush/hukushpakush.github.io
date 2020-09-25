71bada44506839ac718a05707498f645be4313e6
hukushpakush <sharmin.tamanna@lc.cuny.edu>
2020-09-24 22:54:33 -0400

    Updated App.js and App.css to add social link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './Sharmin.jpeg';
 import linkedin from './linkedin.png';
+import twitter from './twitter.png';
 import './App.css';
 
 function App() {
@@ -17,9 +18,23 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Check me out!
+          Check me out on github!
         </a>
-        
+        <div className="Socials">
+           <a
+          className="Linkedin"
+          href="https://linkedin.com/in/sharmint"
+        >
+          <img src={linkedin} alt="linkedin logo" width="43" height="44" ></img>
+        </a>
+        <a
+          className="Twitter"
+          href="https://twitter.com/hukushpakush"
+        >
+          <img src={twitter} alt="twitter logo" width="38" height="38" ></img>
+      
+        </a>
+        </div>
       
       
       </header>
